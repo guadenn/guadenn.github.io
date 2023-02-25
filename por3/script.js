57c318bdb37014deec93ef94ce584ed9668367e3
guadenn <88500096+guadenn@users.noreply.github.com>
2023-02-25 09:06:09 -0300

    1

---
+++ por3/script.js
@@ -0,0 +1,95 @@
+//funcion qe marca seleccionado
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    // desaparece del menu una vez seleccionado
+
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+
+
+//funcion qe muestra menu responsive 
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive"
+    }else{
+        x.className = "";
+    }
+}
+
+//detecto el scrolling para la animacion de las barras/
+
+window.onscroll = function() {
+    efectoHabilidades()
+};
+
+//funcion qe aplica la animacion
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300){
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+}
+
+
+//formulario
+const form = document.getElementById("form")
+const sendMail = document.getElementById("emailA")
+
+
+function handleSendEmail(event){
+    event.preventDefault()
+    const fd = new FormData(this)
+
+
+    sendMail.setAttribute(
+        'href',
+        `mailTo:guadabosio@gmail.com?subject=${fd.get('tema')}&body=${fd.get('message')}`
+    )
+
+
+    sendMail.click()
+}
+
+
+form.addEventListener('submit', handleSendEmail)
+
+// modo oscuro
+const bdark = document.querySelector('#bdark');
+const body = document.querySelector('body');
+
+load(); 
+
+bdark.addEventListener('click', e =>{
+    body.classList.toggle('darkmode');
+    store(body.classList.contains('darkmode'));
+});
+
+function load() {
+    const darkmode = localStorage.getItem('darkmode');
+    
+    if(!darkmode){
+        store('false');
+
+    }else if(darkmode == 'true'){
+        body.classList.add('darkmode');
+    }
+}
+
+function store(value) {
+    localStorage.setItem('darkmode', value);
+}
